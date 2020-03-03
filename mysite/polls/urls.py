de2db59f6088d3ff332d09ab862818948e4816e0
Bruno Dias <bruno.dias@bemobi.com.br>
2020-03-03 17:24:32 -0300

    Test patterns finished. Starting part 6.

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.utils import timezone
 
 from . import views
 
@@ -9,3 +10,11 @@ urlpatterns = [
     path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
     path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
+
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        # Return the last five published questions.
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
